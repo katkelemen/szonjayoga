0fa28a5e4386f0f333d6b4bbc3d6d205009d8410
Kata Kelemen <yourname@apple.com>
2016-05-06 15:50:51 +0100

    library location added to map with clickable address

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,5 @@
 function init_map() {
-		var var_location = new google.maps.LatLng(51.5200801,-0.1388887);
+		var var_location = new google.maps.LatLng(51.5200801, -0.1388887);
 
         var var_mapoptions = {
           center: var_location,
@@ -15,6 +15,18 @@ function init_map() {
             var_mapoptions);
 
 		var_marker.setMap(var_map);
+		
+		$( "#fitzrovia" ).click(function() {
+			var_marker.setPosition(new google.maps.LatLng(51.5200801, -0.1388887));
+			var_map.setZoom(15);
+		    var_map.setCenter(var_marker.getPosition());
+		});
+
+		$( "#library" ).click(function() {
+			var_marker.setPosition(new google.maps.LatLng(51.6135, -0.1534));
+			var_map.setZoom(15);
+		    var_map.setCenter(var_marker.getPosition());
+		});
 
       }
 
